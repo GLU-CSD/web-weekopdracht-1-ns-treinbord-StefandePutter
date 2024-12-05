d4decda0db8d84cc6c55d00149c1002b91015290
StefandePutter <90257174+StefandePutter@users.noreply.github.com>
2024-12-05 01:04:29 +0100

    made api workish

--- assets/js/api.js
+++ assets/js/api.js
@@ -1,45 +1,52 @@
-// fetch(
-// 	"https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/arrivals?station=Ut",
-// 	{
-// 		method: "GET",
-// 		// Request headers
-// 		headers: {
-// 			"Cache-Control": "no-cache",
-// 			"Ocp-Apim-Subscription-Key": "3311bdfc1f4a464fbb0e4cfbcf22ce35",
-// 		},
-// 	}
-// )
-// 	.then((response) => {
-// 		console.log(response.status);
-// 		// console.log(response.text());
-// 		test = response;
-// 	})
-// 	.catch((err) => console.error(err));
-
-async function getData() {
-	try {
-		const response = await fetch(
-			"https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/arrivals?station=Ut",
-			{
-				method: "GET",
-				// Request headers
-				headers: {
-					"Cache-Control": "no-cache",
-					"Ocp-Apim-Subscription-Key": api_key,
-				},
-			}
-		);
-		if (!response.ok) {
-			throw new Error(`Response status: ${response.status}`);
-		}
-
-		const stream = response.body.pipeThrough(new TextDecoderStream());
-		for await (const value of stream) {
-			console.log(value);
-		}
-	} catch (error) {
-		console.error(error.message);
+let test1;
+let test;
+fetch(
+	"https://gateway.apiportal.ns.nl/reisinformatie-api/api/v2/departures?station=Ut",
+	{
+		method: "GET",
+		// Request headers
+		headers: {
+			"Cache-Control": "no-cache",
+			"Ocp-Apim-Subscription-Key": API_KEY,
+		},
 	}
-}
+)
+	.then((response) => {
+		if (!response.ok) {
+			throw new Error(
+				"Network response was not ok. status: " + response.status
+			);
+		}
+		return response.json();
+	})
+	.then((data) => {
+		test1 = data;
+		data.payload.departures.every((element) => {
+			if (
+				element.direction == "Zwolle" &&
+				element.trainCategory == "SPR"
+			) {
+				test = element;
+				makeBoard(test);
+				return false;
+			} else {
+				return true;
+			}
+		});
+	})
+	.catch((err) => console.error(err));
 
-getData();
+function makeBoard(data) {
+	track = data.actualTrack;
+	trainType = data.trainCategory;
+	viaStations = data.routeStations;
+
+	time = new Date(data.actualDateTime) - new Date();
+	time -= data.actualTimeZoneOffset * 60000;
+	time = new Date(time);
+
+	hour = time.getHours();
+	min = time.getMinutes();
+
+	// console.log(viaStations[0].mediumName);
+}
